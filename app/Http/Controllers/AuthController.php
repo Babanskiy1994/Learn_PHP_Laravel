029bc1128f61b54432888bf1f19f0a9e88842567
Aleksey Babanskiy <a.babanskiy@crpo.su>
2022-10-20 18:06:51 +0300

    Нерабочая регистрация

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view("auth.login");
+    }
+
+    public function showRegisterForm()
+    {
+        return view("auth.register");
+    }
+
+    public function register(RegistrationRequest $request)
+    {
+
+
+        dd(1);
+    }
+}
