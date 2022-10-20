d39922e2f199bb2a50da69bb4348370cfb0a4a90
Aleksey Babanskiy <a.babanskiy@crpo.su>
2022-10-20 18:06:51 +0300

    Добавление страниц авторизации и регистрации

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,12 @@
 
 @section('content')
     @include('partials.header')
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 mb-20">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 mb-10">
         @foreach($posts as $post)
             @include('posts.partials.item', ["post" => $post])
         @endforeach
+    </div>
+    <div class="px-6">
+        <a href="{{route("posts.index")}}" class="text-xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Посмотреть все посты</a><br>
     </div>
 @endsection
