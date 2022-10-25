fc67d55c26fa9f353650434e6e95ad499512b74e
Aleksey Babanskiy <a.babanskiy@crpo.su>
2022-10-25 11:55:21 +0300

    Подключение комментариев к постам

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentForm;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -26,5 +27,14 @@ class PostController extends Controller
         return view('posts.show', [
             "post" => $post,
         ]);
+    }
+
+    public function comment($id, CommentForm $request)
+    {
+        $post = Post::findOrFail($id);
+
+        $post->comments()->create($request->validated());
+
+        return redirect(route("posts.show", $id));
     }
 }
